f13e050b84602a9d3a2775a871415a53143e7a13
Daniel Obare Nyakundi <dnobare@gmail.com>
2022-09-14 08:11:56 +0300

    adding records

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Security.create(place: '45km West of the capital', description: 'Attempted human trafficking racket', country: 'Kenya', casualties: 0)
+Security.create(place: 'Outskirts of Wales', description: 'Political uncertainty over election winner', country: 'Wales', casualties: 3)
+Security.create(place: 'Several of Capetown cities', description: 'Suspicious terrorism activities', country: 'South Africa', casualties: 2)
+Security.create(place: '38 degrees North of Mogadishu', description: 'Insurgency', country: 'Somalia', casualties: 1)
