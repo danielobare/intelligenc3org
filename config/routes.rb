ee886a7fadbaa8df65cecbf16b4a70154882f985
Daniel Obare Nyakundi <dnobare@gmail.com>
2022-09-15 06:14:43 +0300

    auto changes commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'homepage/index'
   namespace :api do
     namespace :v1 do
       get 'securities/index'
       post 'securities/create'
-      delete 'securities/id', to: 'securities#destroy'
-    end
+     delete 'securities/id', to: 'securities#destroy'
+   end
+  end
 
-    root 'securities#index'
+  root 'securities#index'
+  For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
